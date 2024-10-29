dcad52d874919ed20c47e8eebef629f55e6f86c5
Imam444 <mdimamhossain565528@gmail.com>
2024-10-30 02:18:54 +0600

    implement fiter and deploy

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -5,7 +5,20 @@ import { getStoredJobApplication } from "../../utility/localstorage";
 const AppliedJobs = () => {
   const jobs = useLoaderData();
     const [appliedJob, setAppliedJob] = useState([]);
-    const [displayJob, setDisplayJob]= useState([],)
+  const [displayJob, setDisplayJob] = useState([]);
+  const handleJobsFilter = filter => {
+    if (filter === 'all') {
+      setDisplayJob(appliedJob)
+    }
+    else if (filter === 'remote') {
+      const remoteJobs = appliedJob.filter(job => job.remote_or_onsite === 'Remote')
+      setDisplayJob(remoteJobs)
+    }
+    else if (filter === 'onsite') {
+      const onsiteJobs = appliedJob.filter(job => job.remote_or_onsite === 'Onsite')
+      setDisplayJob(onsiteJobs)
+    }
+  }
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
     if (jobs.length > 0) {
@@ -23,24 +36,26 @@ const AppliedJobs = () => {
   }, [jobs]);
   return (
     <div>
+      <h2 className="text-2xl">I Applied Jobs:{appliedJob.length}</h2>
       <details className="dropdown">
         <summary className="btn m-1">open or close</summary>
         <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li>
+          <li onClick={() => handleJobsFilter("all")}>
             <a>All</a>
           </li>
-          <li>
-            <a>Remote</a>
-            <a>Onsite</a>
-          </li>
+          <li onClick={() => handleJobsFilter("remote")}> <a>Remote</a></li>
+          
+          <li onClick={() => handleJobsFilter("onsite")}><a>Onsite</a></li>
+            
+          
         </ul>
       </details>
-      <h2 className="text-2xl">I Applied Jobs:{appliedJob.length}</h2>
+
       <ul>
-        {displayJob.appliedJob.map((job) => (
+        {displayJob.map((job) => (
           <li key={job.id}>
             <span>
-              {job.job_title} {job.company_name}:{job.remote_or_onsite}
+              {job.job_title} {job.company_name} :{job.remote_or_onsite}
             </span>
           </li>
         ))}
