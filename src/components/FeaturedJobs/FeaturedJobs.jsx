c7e7a732fbfd9164703f5624704ec987fc9f3f99
Imam444 <mdimamhossain565528@gmail.com>
2024-10-29 15:57:40 +0600

    add icon and handle show all jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,7 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([])
+    const[dataLength, setDataLength]=useState(4)
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
@@ -18,10 +19,13 @@ const FeaturedJobs = () => {
                 <h2 className="text-5xl">Featured Jobs:{ jobs.length}</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div className="grid grid-cols-2">
+            <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job =><Job key={job.id} job={job}></Job>)
+                    jobs.slice(0,dataLength).map(job =><Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength ===jobs.length && 'hidden'}>
+                <button onClick={()=> setDataLength(jobs.length)} className="btn btn-primary">Show All Job</button>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,30 +1,42 @@
-
+import { IoLocationOutline } from "react-icons/io5";
+import { CiDollar } from "react-icons/ci";
 const Job = ({ job }) => {
-  const { logo, job_title, company_name, remote_or_onsite, location,
-      job_type, salary, job_description
-    } = job;
-    return (
-      <div className="card card-compact bg-base-100 shadow-xl">
-        <figure>
-          <img src={logo} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{job_title}</h2>
-          <p>{company_name}</p>
-          <div>
-            <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
-              {remote_or_onsite}
-            </button>
-            <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
-              {job_type}
-            </button>
-          </div>
-          <div className="card-actions ">
-            <button className="btn btn-primary">View Details</button>
-          </div>
+  const {
+    logo,
+    job_title,
+    company_name,
+    remote_or_onsite,
+    location,
+    job_type,
+    salary,
+    job_description,
+  } = job;
+  return (
+    <div className="card card-compact bg-base-100 shadow-xl">
+      <figure>
+        <img src={logo} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{job_title}</h2>
+        <p>{company_name}</p>
+        <div>
+          <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
+            {remote_or_onsite}
+          </button>
+          <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
+            {job_type}
+          </button>
+        </div>
+        <div className="mt-4 flex">
+          <h2 className="flex mr-2"><IoLocationOutline className="text-2xl"></IoLocationOutline>{location}</h2>
+          <h2 className="text-2xl flex"><CiDollar></CiDollar>{ salary}</h2>
+        </div>
+        <div className="card-actions ">
+          <button className="btn btn-primary">View Details</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Job;
