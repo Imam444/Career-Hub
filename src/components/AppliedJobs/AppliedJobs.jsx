96399760a7923b7fc38df272a1705c0ecdd90ee1
Imam444 <mdimamhossain565528@gmail.com>
2024-10-31 02:47:00 +0600

    edit job details and applied jobs

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -2,69 +2,111 @@ import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../utility/localstorage";
 import { Helmet } from "react-helmet-async";
+import Job from "../Job/Job";
 
 const AppliedJobs = () => {
   const jobs = useLoaderData();
-    const [appliedJob, setAppliedJob] = useState([]);
+  const [appliedJob, setAppliedJob] = useState([]);
   const [displayJob, setDisplayJob] = useState([]);
-  const handleJobsFilter = filter => {
-    if (filter === 'all') {
-      setDisplayJob(appliedJob)
+   const [filter, setFilter] = useState("All");
+
+  
+    if (filter === "all") {
+      setDisplayJob(appliedJob);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJob.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJob(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJob.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJob(onsiteJobs);
     }
-    else if (filter === 'remote') {
-      const remoteJobs = appliedJob.filter(job => job.remote_or_onsite === 'Remote')
-      setDisplayJob(remoteJobs)
-    }
-    else if (filter === 'onsite') {
-      const onsiteJobs = appliedJob.filter(job => job.remote_or_onsite === 'Onsite')
-      setDisplayJob(onsiteJobs)
-    }
-  }
+  
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
     if (jobs.length > 0) {
       //
       const jobsApplied = [];
       for (const id of storedJobIds) {
-        const job = jobs.find(job => job.id === id);
+        const job = jobs.find((job) => job.id === id);
         if (job) {
           jobsApplied.push(job);
         }
       }
-        setAppliedJob(jobsApplied);
-        setDisplayJob(jobsApplied)
+      setAppliedJob(jobsApplied);
+      setDisplayJob(jobsApplied);
     }
   }, [jobs]);
   return (
-    <div>
+    <>
       <Helmet>
         <title>Career Hub | Applied Job</title>
       </Helmet>
-      <h2 className="text-2xl">I Applied Jobs:{appliedJob.length}</h2>
-      <details className="dropdown">
-        <summary className="btn m-1">open or close</summary>
-        <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li onClick={() => handleJobsFilter("all")}>
-            <a>All</a>
-          </li>
-          <li onClick={() => handleJobsFilter("remote")}> <a>Remote</a></li>
-          
-          <li onClick={() => handleJobsFilter("onsite")}><a>Onsite</a></li>
-            
-          
-        </ul>
-      </details>
-
-      <ul>
-        {displayJob.map((job) => (
-          <li key={job.id}>
-            <span>
-              {job.job_title} {job.company_name} :{job.remote_or_onsite}
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
+      
+      <div className="container px-6 mx-auto py-16 overflow-hidden">
+        <div className="mb-5 text-right">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn m-1">
+              Filter By
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <button onClick={() => setFilter("all")}>All</button>
+              </li>
+              <li>
+                <button onClick={() => setFilter("Remote")}>
+                  Remote
+                </button>
+              </li>
+              <li>
+                <button onClick={() => setFilter("Onsite")}>
+                  Onsite
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {displayJob.length ? (
+          <div className="flex flex-col gap-4">
+            {displayJob.map((job) => (
+              <Job
+                key={job.id}
+                job={job}
+                setAppliedJobs={setAppliedJob}
+                appliedJobs={appliedJob}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="text-center py-16">
+            <h4 className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-purple-400 inline-block text-transparent bg-clip-text">
+              You didn&apos;t applied for Jobs{filter}
+            </h4>
+          </div>
+        )}
+      </div>
+      ) : (
+      <div className="container mx-auto py-40 text-center">
+        <h4 className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-purple-400 inline-block text-transparent bg-clip-text">
+          You didnt&apos;t applied for any Jobs{filter}
+        </h4>
+        <p className="text-neutral-500 py-2">
+          Apply for jobs to see them listed here.
+        </p>
+        <button
+          onClick={() => setFilter("jobs")}
+          className="btn bg-gradient-to-r from-indigo-400 to-purple-400 text-white px-3 md:px-5 z-50"
+        >
+          Apply Now
+        </button>
+      </div>
+    </>
   );
 };
 
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -3,6 +3,11 @@ import { ToastContainer, toast } from "react-toastify";
 import { useLoaderData, useParams } from "react-router-dom";
 import { saveJobApplication } from "../../utility/localstorage";
 import { Helmet } from "react-helmet-async";
+import { IoLocationOutline } from "react-icons/io5";
+import { CiDollar } from "react-icons/ci";
+import { LuCalendarDays } from "react-icons/lu";
+import { FiPhone } from "react-icons/fi";
+import { HiOutlineEnvelope } from "react-icons/hi2";
 
 const JobDetails = () => {
   const jobs = useLoaderData();
@@ -15,25 +20,108 @@ const JobDetails = () => {
   }
 
   return (
-    <div>
-      <Helmet>
-        <title>
-          Career Hub |  Job Details
-        </title>
-      </Helmet>
-      <div className="grid gap-4 md:grid-cols-4">
-        <div className="border md:col-span-3">
-          <h2>Details Coming here</h2>
-          <h2>Job Details of:{job.job_title}</h2>
-          <p>{job.company_name}</p>
-        </div>
-        <div className="border">
-          <h2 className="text-2xl">Side things</h2>
-          <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
-        </div>
+    <> <Helmet>
+      <title> Career Hub  | Job Details</title>
+     </Helmet>
+      <div className="container px-4 mx-auto py-16 flex flex-col lg:flex-row gap-5 ">
+                <div className="flex flex-col gap-3">
+                    <p className="text-sm text-neutral-500">
+                        <span className="font-bold text-neutral-900">
+                            Job Description:
+                        </span>{" "}
+                        {job.job_description}
+                    </p>
+                    <p className="text-sm text-neutral-500">
+                        <span className="font-bold text-neutral-900">
+                            Job Responsibility:
+                        </span>{" "}
+                        {job.job_responsibility}
+                    </p>
+                    <p className="text-sm font-bold text-neutral-900">
+                        Educational Requirements:
+                    </p>
+                    <p className="text-sm text-neutral-500">
+                        {job.educational_requirements}
+                    </p>
+                    <p className="text-sm font-bold text-neutral-900">
+                        Experiences:
+                    </p>
+                    <p className="text-sm text-neutral-500">
+                        {job.experiences}
+                    </p>
+                </div>
+                <div className="w-full lg:w-8/12">
+                    <div className="bg-[#f4f1ff] p-5 rounded-md">
+                        <h2 className="text-md font-extrabold pb-2 mb-3 border-b-2">
+                            Job Details
+                        </h2>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex gap-1 ">
+                                <CiDollar className="w-5 h-5 text-indigo-400" />
+                                <p className="text-sm text-neutral-500 ">
+                                    <span className="font-bold text-neutral-900">
+                                        Salary :
+                                    </span>{" "}
+                                    {job.salary}
+                                </p>
+                            </div>
+                            <div className="flex gap-1">
+                                <LuCalendarDays className="w-5 h-5 text-indigo-400" />
+                                <p className="text-sm text-neutral-500">
+                                    <span className="font-bold text-neutral-900">
+                                        Job Title :
+                                    </span>{" "}
+                                    {job.job_title}
+                                </p>
+                            </div>
+                        </div>
+                        <h2 className="text-md font-extrabold pb-2 mb-3 border-b-2 pt-3">
+                            Contact Information
+                        </h2>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex gap-1">
+                                <FiPhone className="w-5 h-5 text-indigo-400" />
+                                <p className="text-sm text-neutral-500">
+                                    <span className="font-bold text-neutral-900">
+                                        Phone :
+                                    </span>{" "}
+                                    {job.contact_information.phone}
+                                </p>
+                            </div>
+                            <div className="flex gap-1">
+                                <HiOutlineEnvelope className="w-5 h-5 text-indigo-400" />
+                                <p className="text-sm text-neutral-500">
+                                    <span className="font-bold text-neutral-900">
+                                        Email :
+                                    </span>{" "}
+                                    {job.contact_information.email}
+                                </p>
+                            </div>
+                            <div className="flex gap-1">
+                                <IoLocationOutline className="w-5 h-5 text-indigo-400" />
+                                <p className="text-sm text-neutral-500">
+                                    <span className="font-bold text-neutral-900">
+                                        Address :
+                                    </span>{" "}
+                                    {job.contact_information.address}
+                                </p>
+                            </div>
+                        </div>
+                      </div>
+                      <button
+                        onClick={handleApplyJob}
+                        className="btn btn-block bg-gradient-to-r from-indigo-400 to-purple-400 text-white px-5 mt-3"
+                    >
+                        Apply Now
+                    </button>
+                    <ToastContainer /> 
+             
+          </div>
       </div>
-      <ToastContainer/>
-    </div>
+              
+        
+      
+   </>
   );
 };
 
